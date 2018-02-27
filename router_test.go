d023566858f8b9c66e22f753824bf750e613b800
lufia <lufia@lufia.org>
2018-02-27 12:02:01 +0900

    Add test for race condition of requestContextStore

--- router_test.go
+++ router_test.go
@@ -7,6 +7,7 @@ import (
 	"reflect"
 	"regexp"
 	"strings"
+	"sync"
 	"testing"
 )
 
@@ -956,4 +957,25 @@ func isHandlersSliceDeepEqual(first []http.HandlerFunc, second []http.HandlerFun
 		}
 	}
 	return true
+}
+
+func TestContextStoreRace(t *testing.T) {
+	router := NewRouter()
+	handler := func(res http.ResponseWriter, req *http.Request) {}
+	router.Get("/hello/world", handler)
+
+	server := httptest.NewServer(router)
+	defer server.Close()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			r := httptest.NewRequest("GET", "/hello/world", nil)
+			w := httptest.NewRecorder()
+			router.ServeHTTP(w, r)
+		}()
+	}
+	wg.Wait()
 }
